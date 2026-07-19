106795042b98bd2d7c0b4faa6a34e9f502c4fcfe
martinhugo <12345678+martinhugo@users.noreply.github.com>
2026-07-19 19:37:32 +0200

    fix: coherence fix amongts lerp implementations

--- src/primitives/point.rs
+++ src/primitives/point.rs
@@ -10,18 +10,18 @@ use nalgebra::{Vector2, Vector3, Vector4};
 /// in spline computation, enabling support for 2D, 3D, or any
 /// custom coordinate space.
 pub trait Point {
-    /// Returns the distance between `self` and `other`
-    fn distance(&self, other: &Self) -> f32;
+    /// Returns the distance between `self` and `rhs`
+    fn distance(&self, rhs: &Self) -> f32;
 
-    /// Linearly interpolates between `self` and `other`
+    /// Linearly interpolates between `self` and `rhs`
     ///
-    /// Returns t*self + (1-t)*other, where `t = 0.0` yields
-    /// `other` and `t = 1.0` yields self.
+    /// Returns (1-s)*self + s*rhs, where `s = 0.0` yields
+    /// `rhs` and `t = 1.0` yields self.
     ///
     /// # Panics
     ///
-    /// Implementation may panic if `t` is outside `[0.0, 1.0]`.
-    fn lerp(&self, other: &Self, t: f32) -> Self;
+    /// Implementation may panic if `s` is outside `[0.0, 1.0]`.
+    fn lerp(&self, rhs: &Self, s: f32) -> Self;
 }
 
 // `splinex` internal data structures
@@ -34,19 +34,19 @@ pub struct Point2D {
 }
 
 impl Point for Point2D {
-    fn distance(&self, other: &Self) -> f32 {
-        ((other.x - self.x).powi(2) + (other.y - self.y).powi(2)).sqrt()
+    fn distance(&self, rhs: &Self) -> f32 {
+        ((rhs.x - self.x).powi(2) + (rhs.y - self.y).powi(2)).sqrt()
     }
 
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
 
         Self {
-            x: t * self.x + (1.0 - t) * other.x,
-            y: t * self.y + (1.0 - t) * other.y,
+            x: (1.0-s) * self.x + s * rhs.x,
+            y: (1.0-s) * self.y + s * rhs.y,
         }
     }
 }
@@ -60,21 +60,21 @@ pub struct Point3D {
 }
 
 impl Point for Point3D {
-    fn distance(&self, other: &Self) -> f32 {
-        ((other.x - self.x).powi(2) + (other.y - self.y).powi(2) + (other.z - self.z).powi(2))
+    fn distance(&self, rhs: &Self) -> f32 {
+        ((rhs.x - self.x).powi(2) + (rhs.y - self.y).powi(2) + (rhs.z - self.z).powi(2))
             .sqrt()
     }
 
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
 
         Self {
-            x: t * self.x + (1.0 - t) * other.x,
-            y: t * self.y + (1.0 - t) * other.y,
-            z: t * self.z + (1.0 - t) * other.z,
+            x: (1.0 - s) * self.x + s * rhs.x,
+            y: (1.0 - s) * self.y + s * rhs.y,
+            z: (1.0 - s) * self.z + s * rhs.z,
         }
     }
 }
@@ -83,85 +83,85 @@ impl Point for Point3D {
 
 // glam support
 impl Point for Vec2 {
-    fn distance(&self, other: &Self) -> f32 {
-        (*self - *other).length()
+    fn distance(&self, rhs: &Self) -> f32 {
+        (*self - *rhs).length()
     }
 
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
-        Self::lerp(*self, *other, t)
+        Self::lerp(*self, *rhs, s)
     }
 }
 
 impl Point for Vec3 {
-    fn distance(&self, other: &Self) -> f32 {
-        (*self - *other).length()
+    fn distance(&self, rhs: &Self) -> f32 {
+        (*self - *rhs).length()
     }
 
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
-        Self::lerp(*self, *other, t)
+        Self::lerp(*self, *rhs, s)
     }
 }
 
 impl Point for Vec4 {
-    fn distance(&self, other: &Self) -> f32 {
-        (*self - *other).length()
+    fn distance(&self, rhs: &Self) -> f32 {
+        (*self - *rhs).length()
     }
 
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
-        Self::lerp(*self, *other, t)
+        Self::lerp(*self, *rhs, s)
     }
 }
 
 // nalgebra support
 
 impl Point for Vector2<f32> {
-    fn distance(&self, other: &Self) -> f32 {
-        self.metric_distance(other)
+    fn distance(&self, rhs: &Self) -> f32 {
+        self.metric_distance(rhs)
     }
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
-        self.lerp(other, t)
+        self.lerp(rhs, s)
     }
 }
 
 impl Point for Vector3<f32> {
-    fn distance(&self, other: &Self) -> f32 {
-        self.metric_distance(other)
+    fn distance(&self, rhs: &Self) -> f32 {
+        self.metric_distance(rhs)
     }
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
-        self.lerp(other, t)
+        self.lerp(rhs, s)
     }
 }
 
 impl Point for Vector4<f32> {
-    fn distance(&self, other: &Self) -> f32 {
-        self.metric_distance(other)
+    fn distance(&self, rhs: &Self) -> f32 {
+        self.metric_distance(rhs)
     }
-    fn lerp(&self, other: &Self, t: f32) -> Self {
+    fn lerp(&self, rhs: &Self, s: f32) -> Self {
         assert!(
-            (0.0..=1.0).contains(&t),
-            "`t` must be in [0.0, 1.0], got {t}"
+            (0.0..=1.0).contains(&s),
+            "`s` must be in [0.0, 1.0], got {s}"
         );
-        self.lerp(other, t)
+        self.lerp(rhs, s)
     }
 }
 
@@ -207,14 +207,14 @@ mod tests {
         let a = Point2D { x: 1.0, y: 2.0 };
         let b = Point2D { x: 4.0, y: 6.0 };
 
-        assert_eq!(a.lerp(&b, 1.0), Point2D { x: 1.0, y: 2.0 });
-        assert_eq!(a.lerp(&b, 0.0), Point2D { x: 4.0, y: 6.0 });
+        assert_eq!(a.lerp(&b, 0.0), Point2D { x: 1.0, y: 2.0 });
+        assert_eq!(a.lerp(&b, 1.0), Point2D { x: 4.0, y: 6.0 });
         assert_eq!(a.lerp(&b, 0.5), Point2D { x: 2.5, y: 4.0 });
     }
 
     /// Asserts that lerp panic when given a wrong input parameter for point 2D
     #[test]
-    #[should_panic(expected = "`t` must be in [0.0, 1.0], got 1.1")]
+    #[should_panic(expected = "`s` must be in [0.0, 1.0], got 1.1")]
     fn test_point_2d_lerp_panic() {
         let a = Point2D { x: 1.0, y: 2.0 };
         let b = Point2D { x: 4.0, y: 6.0 };
@@ -286,7 +286,7 @@ mod tests {
         };
 
         assert_eq!(
-            a.lerp(&b, 1.0),
+            a.lerp(&b, 0.0),
             Point3D {
                 x: 4.0,
                 y: 1.0,
@@ -294,7 +294,7 @@ mod tests {
             }
         );
         assert_eq!(
-            a.lerp(&b, 0.0),
+            a.lerp(&b, 1.0),
             Point3D {
                 x: 6.0,
                 y: 4.0,
@@ -313,7 +313,7 @@ mod tests {
 
     /// Asserts that lerp panic when given a wrong input parameter for Point3D
     #[test]
-    #[should_panic(expected = "`t` must be in [0.0, 1.0], got 1.1")]
+    #[should_panic(expected = "`s` must be in [0.0, 1.0], got 1.1")]
     fn test_point_3d_lerp_panic() {
         let a = Point3D {
             x: 4.0,
@@ -340,7 +340,7 @@ mod tests {
 
     /// Asserts that glam::Vec2 panics when a wrong parameter is given to lerp
     #[test]
-    #[should_panic(expected = "`t` must be in [0.0, 1.0], got 1.1")]
+    #[should_panic(expected = "`s` must be in [0.0, 1.0], got 1.1")]
     fn test_basic_vec2d_glam_lerp_panic() {
         let a = Vec2 { x: 1.0, y: 2.0 };
         let b = Vec2 { x: 4.0, y: 6.0 };
@@ -366,7 +366,7 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "`t` must be in [0.0, 1.0], got 1.1")]
+    #[should_panic(expected = "`s` must be in [0.0, 1.0], got 1.1")]
     fn test_basic_vec3d_glam_lerp_panic() {
         let a = Vec3 {
             x: 4.0,
